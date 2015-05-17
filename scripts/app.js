2b5e5d388dfd9744e1f41548b114e8c39a7fd529
Thuong Ho <thuong.t.ho@gmail.com>
2015-05-16 21:45:42 -0700

    use ng-repeat to display the products

--- scripts/app.js
+++ scripts/app.js
@@ -3,13 +3,54 @@
   var app = angular.module('gemStore', []);
 
   app.controller('StoreController', function(){
-    this.product = gem;
+    // this.product = gem;
+    this.products = gems;
   });
 
   var gem = {
     name: 'Emerald',
     price: 4.80,
-    description: 'greeen as a lime'
+    description: 'greeen as a lime',
+    canPurchase: true,
+    isSoldOut: false
   };
+
+  var gems = [
+    {
+      name: 'Emerald',
+      price: 4.80,
+      description: 'greeen as a lime',
+      canPurchase: true,
+      isSoldOut: false
+    },
+    {
+      name: 'Ruby',
+      price: 3.60,
+      description: 'Red as blood',
+      canPurchase: true,
+      isSoldOut: false
+    },
+    { 
+      name: 'Azurite', 
+      price: 2.95,
+      description: 'Blue Rock',
+      canPurchase: true,
+      isSoldOut: false 
+    },
+    { 
+      name: 'Bloodstone', 
+      price: 5.95,
+      description: 'Formed from the blood of a sorcerer',
+      canPurchase: true,
+      isSoldOut: false 
+    },
+    { 
+      name: 'Zircon', 
+      price: 3.95,
+      description: 'Diamond troll',
+      canPurchase: true,
+      isSoldOut: false 
+    }
+  ];
 
 }());
