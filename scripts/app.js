bbbb8d0fa8928d0fdaa9c3031893b6384725825f
Thuong Ho <thuong.t.ho@gmail.com>
2015-05-17 15:22:48 -0700

    create tab controller to display tabs

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,35 @@
     this.products = gems;
   });
 
+  app.controller('PanelController', function(){
+    // ng-init="tab = 1"
+    this.tab = 1;
+    // create a function to set the tab
+    // assign to ng-click panel.selectTab(number)
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    // create a function to compare the select tab
+    this.isSelected = function(tabValue){
+      return this.tab === tabValue;
+    };
+
+  });
+
+  app.controller('TabController', function(){
+    this.tab = 1;
+
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+  });
+
+
   var gem = {
     name: 'Emerald',
     price: 4.80,
@@ -21,35 +50,50 @@
       price: 4.80,
       description: 'greeen as a lime',
       canPurchase: true,
-      isSoldOut: false
+      isSoldOut: false,
+      images: [
+        "images/emerald.png"
+      ]
     },
     {
       name: 'Ruby',
       price: 3.60,
       description: 'Red as blood',
       canPurchase: true,
-      isSoldOut: false
+      isSoldOut: false,
+      images: [
+        "images/ruby.png"
+      ]
     },
     { 
       name: 'Azurite', 
       price: 2.95,
       description: 'Blue Rock',
       canPurchase: true,
-      isSoldOut: false 
+      isSoldOut: false,
+      images: [
+        "images/azurite.png"
+      ] 
     },
     { 
       name: 'Bloodstone', 
       price: 5.95,
       description: 'Formed from the blood of a sorcerer',
       canPurchase: true,
-      isSoldOut: false 
+      isSoldOut: false,
+      images: [
+        "images/bloodstone.png"
+      ] 
     },
     { 
       name: 'Zircon', 
       price: 3.95,
       description: 'Diamond troll',
       canPurchase: true,
-      isSoldOut: false 
+      isSoldOut: false,
+      images: [
+        "images/zircon.png"
+      ] 
     }
   ];
 
---
+++ scripts/panel.js
@@ -0,0 +1,1 @@
+// angular.module('PanelController', function())
